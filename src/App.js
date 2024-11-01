efb70cc37cac149391cf1d0fde9912b01b323e3f
tdin <65420134@kmitl.ac.th>
2024-11-01 19:58:53 +0700

    initial react app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>This is my react app of modtdin.com</p>
+        <p>
+          If you want to learn react more ==>{" "}
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
